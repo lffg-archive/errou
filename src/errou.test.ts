f726e71390e48c257310cc7344b4d5afe09ddb37
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-04-26 16:28:37 -0300

    feat: Add `errou`

---
+++ src/errou.test.ts
@@ -0,0 +1,101 @@
+import { errou } from './errou';
+
+describe('errou', () => {
+  describe('when no error is thrown', () => {
+    it('returns ok:true when no error is thrown', () => {
+      const sut = errou(wontThrow, 1);
+      expect(sut.ok).toBe(true);
+    });
+
+    it('returns the value returned by the function in the data prop', () => {
+      const expected = wontThrow(1).double;
+      const sut = errou(wontThrow, 1);
+      expect(sut.data?.double).toBe(expected);
+    });
+
+    it('returns error:null when ok is true', () => {
+      const sut = errou(wontThrow, 1);
+      expect(sut.ok).toBe(true);
+      expect(sut.error).toBeNull();
+    });
+  });
+
+  describe('when an error is thrown', () => {
+    it('returns ok:false when a error is thrown', () => {
+      const sut = errou(willThrow);
+      expect(sut.ok).toBe(false);
+    });
+
+    it('returns the error throwed by the function in the error prop', () => {
+      const sut = errou(willThrow);
+      expect(sut.error).toBeInstanceOf(Error);
+      expect(sut.error!.message).toBe('err');
+    });
+
+    it('returns data:null when ok is false', () => {
+      const sut = errou(willThrow);
+      expect(sut.ok).toBe(false);
+      expect(sut.data).toBeNull();
+    });
+  });
+
+  describe('async behavior', () => {
+    describe('when no error is thrown', () => {
+      it('returns ok:true when no error is thrown', async () => {
+        const sut = await errou(wontThrowAsync, 1);
+        expect(sut.ok).toBe(true);
+      });
+
+      it('returns the value returned by the function in the data prop', async () => {
+        const expected = (await wontThrowAsync(1)).double;
+        const sut = await errou(wontThrowAsync, 1);
+        expect(sut.data!.double).toBe(expected);
+      });
+
+      it('returns error:null when ok is true', async () => {
+        const sut = await errou(wontThrowAsync, 1);
+        expect(sut.ok).toBe(true);
+        expect(sut.error).toBeNull();
+      });
+    });
+
+    describe('when an error is thrown', () => {
+      it('returns ok:false when a error is thrown', async () => {
+        const sut = await errou(willThrowAsync);
+        expect(sut.ok).toBe(false);
+      });
+
+      it('returns the error throwed by the function in the error prop', async () => {
+        const sut = await errou(willThrowAsync);
+        expect(sut.error).toBeInstanceOf(Error);
+        expect(sut.error!.message).toBe('err');
+      });
+
+      it('returns data:null when ok is false', async () => {
+        const sut = await errou(willThrowAsync);
+        expect(sut.ok).toBe(false);
+        expect(sut.data).toBeNull();
+      });
+    });
+  });
+});
+
+function wontThrow(a: number) {
+  return {
+    double: a * 2
+  };
+}
+
+function willThrow() {
+  throw new Error('err');
+}
+
+function wontThrowAsync(a: number) {
+  return Promise.resolve({
+    double: a * 2
+  });
+}
+
+function willThrowAsync() {
+  return Promise.reject(new Error('err'));
+}
---
+++ src/errou.ts
@@ -0,0 +1,52 @@
+// HELPER TYPES
+// ============
+
+type MaybeUnknownPromise = Promise<unknown> | unknown;
+type FunctionType = (...args: any[]) => MaybeUnknownPromise;
+type Await<T> = T extends Promise<infer U> ? U : T;
+
+// RESULT TYPES
+// ============
+
+type Ok<D> = { ok: true; data: D; error: null };
+type NotOk<E> = { ok: false; data: null; error: E };
+type Status<D, E> = Ok<D> | NotOk<E>;
+
+type Result<D extends MaybeUnknownPromise, E> = D extends Promise<any>
+  ? Promise<Status<Await<D>, E>>
+  : Status<D, E>;
+
+// HELPER FUNCTIONS
+// ================
+
+const ok = <D>(data: D): Ok<D> => ({ ok: true, error: null, data });
+const notOk = <E>(error: E): NotOk<E> => ({ ok: false, error, data: null });
+
+// MAIN API
+// ========
+
+export function errou<
+  E = Error,
+  Fn extends FunctionType = FunctionType,
+  R = ReturnType<Fn>
+>(fn: Fn, ...args: Parameters<Fn>): Result<R, E> {
+  try {
+    const returnedValue = fn(...args) as R;
+
+    if (returnedValue instanceof Promise) {
+      const promise: Promise<Status<R, E>> = new Promise((resolve) => {
+        returnedValue
+          .then((data) => resolve(ok(data)))
+          .catch((error) => resolve(notOk(error)));
+      });
+
+      return promise as Result<R, E>; // must cast
+    }
+
+    const result: Status<R, E> = ok(returnedValue);
+    return result as Result<R, E>; // must cast
+  } catch (error) {
+    const errorResult: Status<null, E> = notOk(error as E);
+    return errorResult as Result<R, E>; // must cast
+  }
+}
